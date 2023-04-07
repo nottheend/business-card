a8e6a3bb977119f836ae9705066c28bedc63721f
nottheend <nottheend@users.noreply.github.com>
2023-04-07 14:47:04 +0200

    do some work on navbar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,15 @@ export default function ProfileStatistics() {
               <MDBIcon icon="home" className="me-2" />
               Home
             </MDBBtn>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
